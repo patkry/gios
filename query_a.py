49b4b827a03ff1ab5a82635821b09ddb47fd60b8
Patrycja <patrycja@debian>
2020-05-07 14:10:41 +0200

    seaborn graphs

--- query_a.py
+++ query_a.py
@@ -23,7 +23,7 @@ LEFT JOIN stations st ON s.station_id=st.id
 LEFT JOIN cities c ON st.city_id=c.id
 WHERE c.province = 'WIELKOPOLSKIE'
 GROUP BY p.id
-ORDER BY sensors_count DESC;"""
+ORDER BY sensors_count;"""
 params_in_w = pd_query(p_count)
 
 """ Data from one day, one parameter and all its sensors in the province. """
@@ -36,5 +36,24 @@ LEFT JOIN cities c ON st.city_id=c.id
 WHERE a.sensor_v IS NOT NULL AND a.date_s BETWEEN '2020-04-29 01:00:00' AND '2020-04-30 01:00:00' AND p.p_name='pył zawieszony PM10';"""
 pm10_day_wielk = pd_query(pm10_d_wiel)
 
+""" Data from one day, all parameters and all its sensors in the province. """
+d_wiel = """SELECT c.c_name || ' ' || st.street as address, p.p_name as parameter, a.date_s as date, a.sensor_v as value
+FROM air_data a
+LEFT JOIN sensors s ON a.sensor_id=s.id
+LEFT JOIN params p ON s.param_id=p.id
+LEFT JOIN stations st ON s.station_id=st.id
+LEFT JOIN cities c ON st.city_id=c.id
+WHERE a.sensor_v IS NOT NULL AND a.date_s BETWEEN '2020-04-29 01:00:00' AND '2020-04-30 01:00:00';"""
+day_wielk = pd_query(d_wiel)
+
+""" Level of air quality through proviences with count of cities with given level.  """
+q_in_p = """SELECT c.province as province, q.level_name as air_quality, COUNT(*) as city_quant
+FROM cities c
+INNER JOIN stations st ON c.id=st.city_id
+RIGHT JOIN quality q ON st.id=q.station_id
+WHERE q.level_name IS NOT NULL AND NOT q.level_name = 'Brak indeksu'
+GROUP BY c.province, q.level_name"""
+quality_in_prov = pd_query(q_in_p)
+
 if __name__ == "__main__":
-    print(pm10_day_wielk.head())
+    print(quality_in_prov)
